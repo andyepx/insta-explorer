4da49b43cd7d58eafb8ea3a03017ab3eb20f3f7b
Andrea Epifani <andrea@andreaepifani.com>
2019-12-02 22:11:38 +1000

    Cleanup and performance

--- app/src/main.ts
+++ app/src/main.ts
@@ -4,6 +4,12 @@ import store from './store';
 
 Vue.config.productionTip = false;
 
+navigator.serviceWorker.register('/service-worker.js')
+    .then((registration) =>
+        console.log(`Service Worker registration complete, scope: '${registration.scope}'`))
+    .catch((error) =>
+        console.log(`Service Worker registration failed with error: '${error}'`));
+
 new Vue({
   store,
   render: (h) => h(App),
